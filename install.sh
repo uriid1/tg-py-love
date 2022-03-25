af0a79dcd2daaa4c7f03920914d12b8563e88c33
uriid1 <70856305+uriid1@users.noreply.github.com>
2022-03-25 05:07:23 +0300

    install

---
+++ install.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+
+#//******************************//#
+#//# Author: by uriid1		    #//#
+#//# license: GNU GPL		    #//#
+#//# telegram: uriid1		    #//#
+#//# Mail: appdurov@gmail.com	#//#
+####****************************####
+
+
+# const
+readonly version="1.0"
+readonly c_error='\033[5m'
+readonly c_yellow='\033[33m'
+readonly c_red='\033[31m'
+readonly c_green='\033[32m'
+readonly c_def='\033[0m'
+
+
+# install python
+echo -e "${c_green}[Установка] ${c_def} Python 3 \n"
+pkg install -y python
+clear
+
+# install Libs for python
+echo -e "${c_green}[Установка] ${c_def} Библиотка telethon для Python \n"
+pip install telethon
+clear
+
+# Sucsess
+echo -e "${c_green}[Установка успешно завершена!] ${c_def}\n"
